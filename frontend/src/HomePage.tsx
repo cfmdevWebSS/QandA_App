774b69b3f7dcbafa703923691009fbec92ad61b8
Lawrence Buta <larry.buta@gmail.com>
2021-09-27 18:48:30 -0400

    Interim Save Chapter 3 9-27-2021

--- frontend/src/HomePage.tsx
+++ frontend/src/HomePage.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import { QuestionList } from './QuestionList';
 import { getUnasweredQuestions } from './QuestionsData';
+import { Page } from './Page';
+import { PageTitle } from './PageTitle';
 
 export const HomePage = () => (
-  <div>
+  <Page>
     <div>
-      <h2>Unanswered Questions</h2>
-      <button>Ask a Question</button>
+      <div>
+        <PageTitle>Unanswered Questions</PageTitle>
+        <button>Ask a Question</button>
+      </div>
+      <QuestionList data={getUnasweredQuestions()} />
     </div>
-    <QuestionList data={getUnasweredQuestions()} />
-  </div>
+  </Page>
 );
