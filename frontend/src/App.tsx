f11ea1d9cdee45c048baab7531e0d207d142f525
Lawrence Buta <larry.buta@gmail.com>
2021-10-04 18:51:28 -0400

    Interim save 10-04-2021

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,18 @@
+/** @jsxImportSource @emotion/react */
+import { css } from '@emotion/react';
 import React from 'react';
-import './App.css';
 import { Header } from './Header';
 import { HomePage } from './HomePage';
 
 function App() {
   return (
-    <div className="App">
+    <div
+      css={css`
+        font-family: 'Segoe UI', 'Helvetica Neue', sans-serif;
+        font-size: 16px;
+        color: #5c5a5a;
+      `}
+    >
       <Header />
       <HomePage />
     </div>
