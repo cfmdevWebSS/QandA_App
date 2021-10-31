b456b89f5ef0aeee7e93fec7b08629846c1e79d4
Lawrence Buta <larry.buta@gmail.com>
2021-10-30 22:21:58 -0400

    Interim Save 10-30-2021

---
+++ Data/IDataRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using QandA.Data.Models;
+
+namespace QandA.Data
+{
+    public interface IDataRepository
+    {
+        IEnumerable<QuestionGetManyResponse> GetQuestions();
+        IEnumerable<QuestionGetManyResponse> GetQuestionsBySearch(string search);
+        IEnumerable<QuestionGetManyResponse> GetUnansweredQuestions();
+        QuestionGetSingleResponse GetQuestion(int QuestionId);
+        bool QuestionExists(int QuestionId);
+        AnswerGetResponse GetAnswer(int AnswerId);
+        QuestionGetSingleResponse PostQuestion(QuestionPostRequest question);
+        QuestionGetSingleResponse PutQuestion(int questionId, QuestionPutRequest question);
+        void DeleteQuestion(int questionId);
+        AnswerGetResponse PostAnswer(AnswerPostRequest answer);
+    }
+}
